d0ae0ff3d9ac7e3681a810ecaeb73de907f422e5
Rahul Senna <rahulvsenna@gmail.com>
2026-01-24 22:43:21 +0530

    3. EFCore in ASP.NET(Razor Pages)

--- cs/EFCore/Razor_Pages_EF_Core_ASP_NET_Core/ContosoUniversity/Pages/Students/Create.cshtml.cs
+++ cs/EFCore/Razor_Pages_EF_Core_ASP_NET_Core/ContosoUniversity/Pages/Students/Create.cshtml.cs
@@ -27,18 +27,21 @@ namespace ContosoUniversity.Pages.Students
         [BindProperty]
         public Student Student { get; set; } = default!;
 
-        // For more information, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
+				{
+						var emptyStudent = new Student();
 
-            _context.Students.Add(Student);
-            await _context.SaveChangesAsync();
+						if (await TryUpdateModelAsync<Student>(
+								emptyStudent,
+								"student",   // Prefix for form value.
+								s => s.FirstMidName, s => s.LastName, s => s.EnrollmentDate))
+						{
+								_context.Students.Add(emptyStudent);
+								await _context.SaveChangesAsync();
+								return RedirectToPage("./Index");
+						}
 
-            return RedirectToPage("./Index");
-        }
+						return Page();
+				}
     }
 }
--- cs/EFCore/Razor_Pages_EF_Core_ASP_NET_Core/ContosoUniversity/Pages/Students/Edit.cshtml.cs
+++ cs/EFCore/Razor_Pages_EF_Core_ASP_NET_Core/ContosoUniversity/Pages/Students/Edit.cshtml.cs
@@ -24,50 +24,44 @@ namespace ContosoUniversity.Pages.Students
         public Student Student { get; set; } = default!;
 
         public async Task<IActionResult> OnGetAsync(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+				{
+						if (id == null)
+						{
+								return NotFound();
+						}
 
-            var student =  await _context.Students.FirstOrDefaultAsync(m => m.ID == id);
-            if (student == null)
-            {
-                return NotFound();
-            }
-            Student = student;
-            return Page();
-        }
+						Student = await _context.Students.FindAsync(id);
+
+						if (Student == null)
+						{
+								return NotFound();
+						}
+						return Page();
+				}
+
 
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more information, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
+        public async Task<IActionResult> OnPostAsync(int id)
+				{
+						var studentToUpdate = await _context.Students.FindAsync(id);
 
-            _context.Attach(Student).State = EntityState.Modified;
+						if (studentToUpdate == null)
+						{
+								return NotFound();
+						}
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!StudentExists(Student.ID))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+						if (await TryUpdateModelAsync<Student>(
+								studentToUpdate,
+								"student",
+								s => s.FirstMidName, s => s.LastName, s => s.EnrollmentDate))
+						{
+								await _context.SaveChangesAsync();
+								return RedirectToPage("./Index");
+						}
 
-            return RedirectToPage("./Index");
-        }
+						return Page();
+				}
 
         private bool StudentExists(int id)
         {
