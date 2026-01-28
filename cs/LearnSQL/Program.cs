6d3f5b837b1af3bd4909326ecd93b07a7e323350
Rahul Senna <rahulvsenna@gmail.com>
2026-01-28 23:03:04 +0530

    2. Learning SQL

--- cs/LearnSQL/Program.cs
+++ cs/LearnSQL/Program.cs
@@ -9,26 +9,48 @@ await using var connection = new SqliteConnection("Data Source=MyDatabse.db");
 		await connection.OpenAsync();
 		Console.WriteLine("Database connection opened successfuly");
 
-		// var mockDataSql = await File.ReadAllTextAsync("../../../MOCK_DATA.sql");
-		// var command = connection.CreateCommand();
-		// command.CommandText = mockDataSql;
-		// await command.ExecuteNonQueryAsync();
+		// await LoadData("Products.sq");
+		await ReadAllProducts();
 
-		var command = connection.CreateCommand();
-		command.CommandText = @"SELECT Id, first_name AS Name FROM Users";
-
-		var reader = command.ExecuteReader();
-
-		while(reader.Read())
-		{
-			int id = reader.GetInt32(0);
-			string name = reader.GetString(1);
-			Console.Error.WriteLine($"Id: {id}, Name: {name}"); 
-		}
-
-
-	} catch (SqliteException ex)
-	{
-		Console.Error.WriteLine($"SqliteException Message: {ex.Message}"); 
 	}
+	catch (SqliteException ex)
+	{
+		Console.Error.WriteLine($"SqliteException Message: {ex.Message}");
+	}
+}
+
+async Task LoadData(string dbfile)
+{
+	var mockDataSql = await File.ReadAllTextAsync($"../../../{dbfile}");
+	var seedCommand = connection.CreateCommand();
+	seedCommand.CommandText = mockDataSql;
+	await seedCommand.ExecuteNonQueryAsync();
+}
+
+async Task ReadAllProducts()
+{
+	var allProductsSql = connection.CreateCommand();
+	allProductsSql.CommandText = @"SELECT name, category_name, price, price * 1.1
+																	FROM Products
+																	LIMIT -1
+																	";
+
+	var reader = await allProductsSql.ExecuteReaderAsync();
+
+
+	Console.Error.WriteLine(new string('-', 110));
+	Console.Error.WriteLine($"{"Name",-50} | {"Category",-30} | {"Price",-12} | {"New Price",-12}");
+	Console.Error.WriteLine(new string('-', 110));
+
+	while (await reader.ReadAsync())
+	{
+		string productName = reader.GetString(0);
+		string categoryName = reader.GetString(1);
+		float price = reader.GetFloat(2);
+		float newPrice = reader.GetFloat(3);
+
+		Console.Error.WriteLine($"{productName,-50} | {categoryName,-30} | {price,-12:F2} | {newPrice,-12:F2}");
+	}
+
+	Console.Error.WriteLine(new string('-', 110));
 }
