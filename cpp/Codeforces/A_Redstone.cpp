23292926a05aaf5fd1b5009c8f8c0dd3afa5abac
Rahul Senna <rahulvsenna@gmail.com>
2026-02-09 14:54:50 +0530

    2133A	Redstone?  (Codeforces)

---
+++ cpp/Codeforces/A_Redstone.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef int64_t s64;
+typedef uint64_t u64;
+typedef long double r64;
+
+inline void setup()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout), freopen("error.txt", "w", stderr);
+#endif
+}
+
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+void solve();
+int main()
+{
+	setup();
+
+	s64 t;
+	cin >> t;
+
+	while (t--)
+	{
+		solve();
+	}
+
+	return 0;
+}
+
+void solve()
+{
+	int n;
+	cin >> n;
+
+	set<int> nums;
+	int e;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> e;
+		nums.insert(e);
+	}
+
+	if (nums.size()< n)
+		cout << "YES\n";
+	else
+		cout << "NO\n";
+}
