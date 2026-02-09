1b499100a052c596511aed908272804a8bc63c74
Rahul Senna <rahulvsenna@gmail.com>
2026-02-09 15:54:28 +0530

    1690C - Restoring the Duration of Tasks

---
+++ cpp/Codeforces/C_Restoring_the_Duration_of_Tasks.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef int64_t s64;
+typedef uint64_t u64;
+typedef long double r64;
+
+inline void setup()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout), freopen("error.txt", "w", stderr);
+#endif
+}
+
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+void solve();
+int main()
+{
+	setup();
+
+	s64 t;
+	cin >> t;
+
+	while (t--)
+	{
+		solve();
+	}
+
+	return 0;
+}
+
+void solve()
+{
+	int n;
+	cin >> n;
+	vector<s64> s(n), f(n + 1);
+	for (auto &e : s)
+		cin >> e;
+	for (int i = 1; i < f.size(); ++i)
+		cin >> f[i];
+
+	for (int i = 1; i < f.size(); ++i)
+		cout << f[i] - max(f[i - 1], s[i - 1]) << ' ';
+
+	cout << '\n';
+}
