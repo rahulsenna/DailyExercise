ce98a464cb1d80b834ce57c25a912c9509d3f30a
Rahul Senna <rahulvsenna@gmail.com>
2026-02-10 18:22:18 +0530

    1497A Meximization

---
+++ cpp/Codeforces/A_Meximization.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef int64_t s64;
+typedef uint64_t u64;
+typedef long double r64;
+
+inline void setup()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout), freopen("error.txt", "w", stderr);
+#endif
+}
+
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+void solve();
+int main()
+{
+	setup();
+
+	s64 t;
+	cin >> t;
+
+	while (t--)
+	{
+		solve();
+	}
+
+	return 0;
+}
+
+void solve()
+{
+	int n, a;
+	cin >> n;
+	std::unordered_map<int, int> freq;
+
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> a;
+		freq[a]++;
+	}
+
+	std::vector<std::pair<int, int>> nums{freq.begin(), freq.end()};
+	std::ranges::sort(nums, [](auto a, auto b) { return a.first < b.first; });
+
+	std::vector<int> remaining;
+	for (auto [k, v] : nums)
+	{
+		cout << k << ' ';
+		for (int i = 1; i < v; ++i)
+			remaining.push_back(k);
+	}
+
+	for (auto e : remaining)
+		cout << e << ' ';
+	cout << '\n';
+}
