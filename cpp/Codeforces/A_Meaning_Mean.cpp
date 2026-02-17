7e08fe9d5950c6de3a10a118897eb29538ca5efb
Rahul Senna <rahulvsenna@gmail.com>
2026-02-17 21:41:56 +0530

    2021A - Meaning Mean

---
+++ cpp/Codeforces/A_Meaning_Mean.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef int64_t s64;
+typedef uint64_t u64;
+typedef long double r64;
+
+inline void setup()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout), freopen("error.txt", "w", stderr);
+#endif
+}
+
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+void solve();
+int main()
+{
+	setup();
+
+	s64 t;
+	cin >> t;
+
+	while (t--)
+	{
+		solve();
+	}
+
+	return 0;
+}
+
+void solve()
+{
+	int n;
+	cin >> n;
+	multiset<s64> nums;
+	for (int i = 0; i < n; ++i)
+	{
+		s64 a;
+		cin >> a;
+		nums.insert(a);
+	}
+
+	while (nums.size() > 1)
+	{
+		s64 a = *nums.begin();
+		nums.erase(nums.begin());
+		s64 b = *nums.begin();
+		nums.erase(nums.begin());
+
+		nums.insert((a + b) / 2LL);
+	}
+
+	cout << *nums.begin() << '\n';
+}
