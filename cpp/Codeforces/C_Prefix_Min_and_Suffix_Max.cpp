5d7e8e47bd0cef9a6be2455b0d40877b48a91255
Rahul Senna <rahulvsenna@gmail.com>
2026-02-27 19:50:02 +0530

    2123C - Prefix Min and Suffix Max

---
+++ cpp/Codeforces/C_Prefix_Min_and_Suffix_Max.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef int64_t s64;
+typedef uint64_t u64;
+typedef long double r64;
+
+inline void setup()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout), freopen("error.txt", "w", stderr);
+#endif
+}
+
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+void solve();
+int main()
+{
+	setup();
+
+	s64 t;
+	cin >> t;
+
+	while (t--)
+	{
+		solve();
+	}
+
+	return 0;
+}
+
+void solve()
+{
+  int n;
+  cin >> n;
+
+  std::vector<int> a(n);
+  for (auto &e : a)
+    cin >> e;
+
+  std::priority_queue<int, std::vector<int>, std::greater<int>> min_heap;
+  std::priority_queue<int, std::vector<int>>                    max_heap;
+
+  string res(n, '0');
+  for (int i = 0, j = n - 1; i < n; ++i, --j)
+  {
+    min_heap.push(a[i]);
+    max_heap.push(a[j]);
+    if (min_heap.top() >= a[i])
+      res[i] = '1';
+    if (max_heap.top() <= a[j])
+      res[j] = '1';
+  }
+
+  cout << res << '\n';
+}
