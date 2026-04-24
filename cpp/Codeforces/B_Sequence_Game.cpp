888dbbabf46e5cd0ec1c088608e7d2bb1267ebd5
Rahul Senna <rahulvsenna@gmail.com>
2026-04-24 18:56:22 +0530

    1862B - Sequence Game

---
+++ cpp/Codeforces/B_Sequence_Game.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef int64_t s64;
+typedef uint64_t u64;
+typedef long double r64;
+
+inline void setup()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout), freopen("error.txt", "w", stderr);
+#endif
+}
+
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+void solve();
+int main()
+{
+	setup();
+
+	s64 t;
+	cin >> t;
+
+	while (t--)
+	{
+		solve();
+	}
+
+	return 0;
+}
+
+void solve()
+{
+  int m;
+  cin >> m;
+  std::vector<int> res;
+  res.reserve(m * 2);
+  int prev = 0;
+  for (int a, i = 0; i < m; ++i)
+  {
+    cin >> a;
+    if (a < prev)
+      res.push_back(a);
+    res.push_back(a);
+    prev = a;
+  }
+  cout << res.size() << '\n';
+  for (auto e : res)
+    cout << e << ' ';
+  cout << '\n';
+}
