1c33db4450a9356f346c028ab11972a00fae0b49
Rahul Senna <rahulvsenna@gmail.com>
2026-04-24 16:13:06 +0530

    1866A - Ambitious Kid

---
+++ cpp/Codeforces/A_Ambitious_Kid.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef int64_t s64;
+typedef uint64_t u64;
+typedef long double r64;
+
+inline void setup()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout), freopen("error.txt", "w", stderr);
+#endif
+}
+
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+void solve();
+int main()
+{
+	setup();
+
+	solve();
+	return 0;
+}
+
+void solve()
+{
+  int n;
+  cin >> n;
+  int m = INT_MAX;
+  for (int a, i = 0; i < n; ++i)
+  {
+    cin >> a;
+    m = min(m, abs(a));
+  }
+
+  cout << m << '\n';
+}
