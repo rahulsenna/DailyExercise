065bfa06dfbe3e852f0154b945a6cf2ba6892cf8
Rahul Senna <rahulvsenna@gmail.com>
2026-04-27 23:44:01 +0530

    1837A - Grasshopper on a Line

---
+++ cpp/Codeforces/A_Grasshopper_on_a_Line.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef int64_t s64;
+typedef uint64_t u64;
+typedef long double r64;
+
+inline void setup()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout), freopen("error.txt", "w", stderr);
+#endif
+}
+
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+void solve();
+int main()
+{
+	setup();
+
+	s64 t;
+	cin >> t;
+
+	while (t--)
+	{
+		solve();
+	}
+
+	return 0;
+}
+
+void solve()
+{
+  int n, k;
+  cin >> n >> k;
+  if (n % k != 0)
+  {
+    cout << "1\n"
+         << n << '\n';
+    return;
+  }
+  cout << "2\n"
+       << n - 1 << " 1\n";
+}
