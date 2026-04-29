94088e60d89f4e9849e960be5f3862bd7ab45e5b
Rahul Senna <rahulvsenna@gmail.com>
2026-04-29 19:17:42 +0530

    1789A - Serval and Mocha's Array

---
+++ cpp/Codeforces/A_Serval_and_Mocha_s_Array.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef int64_t s64;
+typedef uint64_t u64;
+typedef long double r64;
+
+inline void setup()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout), freopen("error.txt", "w", stderr);
+#endif
+}
+ 
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+void solve();
+int main()
+{
+	setup();
+
+	s64 t;
+	cin >> t;
+
+	while (t--)
+	{
+		solve();
+	}
+
+	return 0;
+}
+
+void solve()
+{
+  int n;
+  cin >> n;
+
+  std::vector<int> a(n);
+  for (auto &e : a)
+    cin >> e;
+
+  for (int i = 0; i < n; ++i)
+  {
+    for (int j = 0; j < n; ++j)
+    {
+      if (std::gcd(a[i], a[j]) <= 2)
+      {
+        cout << "Yes\n";
+        return;
+      }
+    }
+  }
+
+  cout << "No\n";
+}
