280a9d2aa574dc80f72f66d81c12f863f1524adf
Rahul Senna <rahulvsenna@gmail.com>
2026-04-30 12:02:36 +0530

    1788A - One and Two

---
+++ cpp/Codeforces/A_One_and_Two.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef int64_t s64;
+typedef uint64_t u64;
+typedef long double r64;
+
+inline void setup()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout), freopen("error.txt", "w", stderr);
+#endif
+}
+
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+void solve();
+int main()
+{
+	setup();
+
+	s64 t;
+	cin >> t;
+
+	while (t--)
+	{
+		solve();
+	}
+
+	return 0;
+}
+
+void solve()
+{
+  int n;
+  cin >> n;
+
+  std::vector<int> pos;
+  for (int e, i = 1; i <= n; ++i)
+  {
+    cin >> e;
+    if (e == 2)
+      pos.push_back(i);
+  }
+
+  if (pos.size() & 1)
+  {
+    cout << "-1\n";
+    return;
+  }
+  if (pos.empty())
+  {
+    cout << "1\n";
+    return;
+  }
+
+  cout << pos[pos.size() / 2 - 1] << '\n';
+}
