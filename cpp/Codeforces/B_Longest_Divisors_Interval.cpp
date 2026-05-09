9fa781a61a62038bb46eb422c15bdf6f135bb9c3
Rahul Senna <rahulvsenna@gmail.com>
2026-05-09 17:16:47 +0530

    1855B - Longest Divisors Interval

---
+++ cpp/Codeforces/B_Longest_Divisors_Interval.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef uint32_t u32; typedef int64_t s64; typedef uint64_t u64; typedef long double r64;
+inline void setup()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout), freopen("error.txt", "w", stderr);
+#endif
+}
+
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+void solve();
+int main()
+{
+	setup();
+	uint64_t t;
+	cin >> t;
+	while (t--)
+		solve();
+	return 0;
+}
+
+void solve()
+{
+  u64 n;
+  cin >> n;
+  u64 res = 0;
+  while (n % ++res == 0);
+  std::cout << res - 1 << '\n';
+}
