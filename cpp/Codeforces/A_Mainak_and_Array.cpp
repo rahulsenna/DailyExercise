1b9b475c7bcd0801f111462a342aff9f5d03ea95
Rahul Senna <rahulvsenna@gmail.com>
2026-05-14 10:07:10 +0530

    1726A - Mainak and Array

---
+++ cpp/Codeforces/A_Mainak_and_Array.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef uint32_t u32; typedef int64_t s64; typedef uint64_t u64; typedef long double r64;
+inline void setup()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout), freopen("error.txt", "w", stderr);
+#endif
+}
+
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+void solve();
+int main()
+{
+	setup();
+	uint64_t t;
+	cin >> t;
+	while (t--)
+		solve();
+	return 0;
+}
+
+void solve()
+{
+  int n;
+  cin >> n;
+
+  std::vector<int> a(n);
+
+  int mn = INT_MAX, mx = 0;
+  for (int i = 0; i < n; ++i)
+  {
+    cin >> a[i];
+    mn = min(mn, a[i]);
+    mx = max(mx, a[i]);
+  }
+
+  int res = a[n - 1] - mn;
+  res     = max(res, mx - a[0]);
+  for (int i = 1; i < n; ++i)
+    res = max(res, a[i - 1] - a[i]);
+
+  std::cout << res << '\n';
+}
