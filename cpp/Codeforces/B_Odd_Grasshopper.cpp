9f77cc10b42e9813b94341ede88f6087bd0d20ed
Rahul Senna <rahulvsenna@gmail.com>
2026-05-23 12:17:40 +0530

    1607B - Odd Grasshopper

---
+++ cpp/Codeforces/B_Odd_Grasshopper.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef uint32_t u32; typedef int64_t s64; typedef uint64_t u64; typedef long double r64;
+inline void setup() {
+  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout), freopen("error.txt", "w", stderr);
+#endif
+}
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+void solve();
+int main()
+{
+	setup();
+	uint64_t t;
+	cin >> t;
+	while (t--)
+		solve();
+	return 0;
+}
+
+void solve()
+{
+  s64 x, n;
+  cin >> x >> n;
+
+  s64 N = n - (n % 4) + 1;
+
+  for (s64 i = N; i <= n; ++i)
+  {
+    if (x & 1)
+      x += i;
+    else
+      x -= i;
+  }
+  std::cout << x << '\n';
+}
