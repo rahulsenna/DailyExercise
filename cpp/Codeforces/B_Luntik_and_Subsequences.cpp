7178d6832be0873d7173715392d65cf7cd017596
Rahul Senna <rahulvsenna@gmail.com>
2026-05-27 05:09:20 +0530

    1582B - Luntik and Subsequences

---
+++ cpp/Codeforces/B_Luntik_and_Subsequences.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef uint32_t u32; typedef int64_t s64; typedef uint64_t u64; typedef long double r64;
+inline void setup() {
+  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout), freopen("error.txt", "w", stderr);
+#endif
+}
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+void solve();
+int main()
+{
+	setup();
+	uint64_t t;
+	cin >> t;
+	while (t--)
+		solve();
+	return 0;
+}
+
+void solve()
+{
+  int n;
+  cin >> n;
+  int cnt1 = 0, cnt0 = 0;
+  for (int a, i = 0; i < n; ++i)
+  {
+    cin >> a;
+    if (a == 0) cnt0++;
+    if (a == 1) cnt1++;
+  }
+
+  cout << cnt1 * (1LL << cnt0) << '\n';
+}
