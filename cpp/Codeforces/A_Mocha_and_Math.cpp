414eed16a73f60c651f9a1f4caaebe4d423abc3d
Rahul Senna <rahulvsenna@gmail.com>
2026-05-28 03:49:32 +0530

    1559A - Mocha and Math          9

---
+++ cpp/Codeforces/A_Mocha_and_Math.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef uint32_t u32; typedef int64_t s64; typedef uint64_t u64; typedef long double r64;
+inline void setup() {
+  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout), freopen("error.txt", "w", stderr);
+#endif
+}
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+void solve();
+int main()
+{
+	setup();
+	uint64_t t;
+	cin >> t;
+	while (t--)
+		solve();
+	return 0;
+}
+
+void solve()
+{
+  int n, res;
+  cin >> n >> res;
+  for (int a, i = 1; i < n; ++i)
+  {
+    cin >> a;
+    res &= a;
+  }
+  std::cout << res << '\n';
+}
