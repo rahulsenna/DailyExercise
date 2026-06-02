8f4dbd73e6dcf45e451f5f17d9be16971e899ad3
Rahul Senna <rahulvsenna@gmail.com>
2026-06-02 20:02:44 +0530

    1440B - Sum of Medians

---
+++ cpp/Codeforces/B_Sum_of_Medians.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef uint32_t u32; typedef int64_t s64; typedef uint64_t u64; typedef long double r64;
+inline void setup()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout), freopen("error.txt", "w", stderr);
+#endif
+}
+
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+void solve();
+int main()
+{
+	setup();
+	uint64_t t;
+	cin >> t;
+	while (t--)
+		solve();
+	return 0;
+}
+
+void solve()
+{
+  int n, k;
+  cin >> n >> k;
+
+  std::vector<int> a(n * k);
+  for (auto &e : a)
+    cin >> e;
+
+  int step = n / 2 + 1;
+  int median_idx = n * k - step;
+  u64 res = 0;
+  for (int i = 0; i < k; ++i)
+  {
+    res += a[median_idx];
+    median_idx -= step;
+  }
+
+  std::cout << res << '\n';
+}
+
+// [B. Sum of Medians] (https://codeforces.com/problemset/problem/1440/B)
+// [2026-06-01] [18:33:59]
