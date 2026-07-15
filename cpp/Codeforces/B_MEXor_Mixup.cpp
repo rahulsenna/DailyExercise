1323db8d62de8cae20f8cd14f3d4575bb4f1fcea
Rahul Senna <rahulvsenna@gmail.com>
2026-07-15 21:05:17 +0530

    1567B - MEXor Mixup

---
+++ cpp/Codeforces/B_MEXor_Mixup.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef uint32_t u32; typedef int64_t s64; typedef uint64_t u64; typedef long double r64;
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+void solve();
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout), freopen("error.txt", "w", stderr);
+#endif
+	uint64_t t;
+	cin >> t;
+	while (t--)
+		solve();
+	return 0;
+}
+
+void solve()
+{
+  int a, b;
+  cin >> a >> b;
+  int n = a - 1;
+
+  int table[4] = {n, 1, n + 1, 0};
+  int x = table[n % 4];
+
+  if (x == b)
+    cout << a << '\n';
+  else if ((x ^ b) == a)
+    cout << a + 2 << '\n';
+  else
+    cout << a + 1 << '\n';
+}
+
+// [B. MEXor Mixup] (https://codeforces.com/problemset/problem/1567/B)
+// [2026-07-15] [20:15:49]
