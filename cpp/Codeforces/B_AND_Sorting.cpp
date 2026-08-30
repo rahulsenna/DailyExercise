43effb68d02407495a24af5ccb0648fae7628aaf
Rahul Senna <rahulvsenna@gmail.com>
2026-08-30 19:22:57 +0530

    1682B - AND Sorting

---
+++ cpp/Codeforces/B_AND_Sorting.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef uint32_t u32; typedef int64_t s64; typedef uint64_t u64; typedef long double r64;
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+void solve();
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout), freopen("error.txt", "w", stderr);
+#endif
+	uint64_t t;
+	cin >> t;
+	while (t--)
+		solve();
+	return 0;
+}
+
+void solve()
+{
+  int n;
+  cin >> n;
+
+  int res = 0xFFFFFFFF;
+
+  for (int e, i = 0; i < n; ++i)
+  {
+    cin >> e;
+    if (i != e)
+      res &= i;
+  }
+  std::cout << res << '\n';
+}
+
+// [B. AND Sorting] (https://codeforces.com/problemset/problem/1682/B)
+// [2026-08-30] [19:06:26]
